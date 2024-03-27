8926c3cab121394717d62cbfd027ebee9f86c1c6
A-c-e-1 <amanichiseka@gmail.com>
2024-03-27 03:57:08 +0300

    did it

---
+++ Day 12/string.md
@@ -0,0 +1,8 @@
+Day 12 March 26<sup>th
+
+***Todays progress:*** Strings can be written with double or single quotes
+we have many methods to know more info or edit our strings
+
+***Thoughts:*** Python Strings Are unique they have -ve indexes.
+
+***link*** 
---
+++ Day 12/string.py
@@ -0,0 +1,16 @@
+day = "python for intermediate"
+print(day)
+print(day[:])
+print(day[1:-1])
+
+first = "John"
+second = "simon"
+msg = f'{first} [{second}] is a coder'
+print(msg)
+
+course = 'python for beginners'
+print(len(course))
+print(course.upper())
+print(course.find('o'))
+print(course.replace('p', 'j'))
+print("Python" in course)
